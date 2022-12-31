5a3b4b770f3d6c9de80f3beda8ccedf2d474dc99
samuelGodad <117444585+samuelGodad@users.noreply.github.com>
2022-12-31 13:54:16 +0300

    Create CheckLeapYear.py

---
+++ CheckLeapYear.py
@@ -0,0 +1,11 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if ((year%400==0)or (year%100!=0 )and (year%4==0)):
+        leap=True
+        return leap
+    
+    return leap
+
+year = int(input())
